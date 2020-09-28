cd0ec137b1cd716256134445fdfaae9235b1e8d5
rajam-coder <rajamucsmdy@gmail.com>
2020-09-28 22:37:38 +0630

    Initial Commit

---
+++ precache-manifest.2cc08182998161ab8bd92cd6bddba9d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efda1a9c6a52b36143bd26e2b3920609",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37a2fefdc6fc4f1c1583",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ed1e808a0f850eb91c86",
+    "url": "/static/js/2.8a12f3d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8a12f3d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a2fefdc6fc4f1c1583",
+    "url": "/static/js/main.2e7c91c1.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);
